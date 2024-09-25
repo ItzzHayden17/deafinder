9c20f64b4bfe77f3fbfdb13140e7b0a74271c8e3
itzzhayden17 <sony.anray743@gmail.com>
2024-09-25 18:45:06 +0200

    Adding serviceworker

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import Favorite from './pages/Favorite';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { register } from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -18,4 +18,4 @@ root.render(
   </React.StrictMode>
 );
 
-register()
+serviceWorkerRegistration.register();
