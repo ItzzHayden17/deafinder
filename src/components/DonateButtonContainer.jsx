ee4654fa0d88deff2332033e9eb30dcc8fa59050
ItzzHayden17 <sony.anray743@gmail.com>
2024-12-09 20:51:58 +0200

    Adding bitcoin as donate option

--- src/components/DonateButtonContainer.jsx
+++ src/components/DonateButtonContainer.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion'
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
 
 const DonateButtonContainer = (props) => {
+
+  
+const [copy,setCopy] = useState(false)
+
     function handleClose(){
         props.onClick(false)
     }
+
+const handleCopy = () =>{
+  setCopy(true)
+  setTimeout(()=>{
+    setCopy(false)
+  },4000)
+}
   return (
     <div className='DonateButtonContainer'>
         <span onClick={handleClose}>x</span>
@@ -16,6 +29,7 @@ const DonateButtonContainer = (props) => {
         <div className="buttons">
         <a href="https://paypal.me/deafinder?country.x=ZA&locale.x=en_US" className='paypal'><i class="bi bi-paypal"></i> Paypal</a>
         <a href="https://www.buymeacoffee.com/sonyanray7m" className='card'><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=🙌&slug=sonyanray7m&button_colour=5F7FFF&font_colour=ffffff&font_family=Lato&outline_colour=000000&coffee_colour=FFDD00" /></a>
+        <CopyToClipboard text="your-crypto-address-here" onCopy={handleCopy}><a className='crypto' ><i class="bi bi-currency-bitcoin"></i>Crypto Address <div className='tooltip'><i class="bi bi-info"></i>{copy ? <>Adress copied!</>:<>Click to copy address!</> }</div></a></CopyToClipboard>
         </div>
 
     </motion.div>
